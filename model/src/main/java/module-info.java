e61a227be518fb01ad1665ee9b5e21b1abe3c901
ergouser <jredman@ergotech.com>
2025-01-21 12:16:28 -0700

    Removed dependencies

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -2,9 +2,5 @@
  * Module info for Model
  */
 module com.ergotech.grapheditor.model {
-  requires javafx.base;
-  requires javafx.controls;
-  requires java.desktop;
-
   exports com.ergotech.grapheditor.model;
 }
