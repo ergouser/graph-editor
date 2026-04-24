6509e111f395cf292b5f40ac7095aa70ea70632b
ergouser <jredman@ergotech.com>
2026-04-24 13:42:52 -0600

    Vetoable Change

--- api/src/main/java/com/ergotech/grapheditor/model/command/SetPropertyCommand.java
+++ api/src/main/java/com/ergotech/grapheditor/model/command/SetPropertyCommand.java
@@ -1,6 +1,7 @@
 package com.ergotech.grapheditor.model.command;
 
-import java.beans.PropertyChangeListener;
+import java.beans.PropertyVetoException;
+import java.beans.VetoableChangeListener;
 import java.util.Objects;
 import java.util.function.BiFunction;
 
@@ -10,13 +11,13 @@ import javafx.beans.property.adapter.JavaBeanObjectPropertyBuilder;
 
 public class SetPropertyCommand<V> extends AbstractCommand {
   private final Property<V> property;
-  private final V newValue;
+  private V newValue; // can be updated during undo() to support redo() after undo()
   private V oldValue;
 
   // Optional, for temporary binding during execute/undo
   private final Object listenerTarget;
-  private final PropertyChangeListener listener;
-  private final BiFunction<Object, PropertyChangeListener, Runnable> listenerBinder;
+  private final VetoableChangeListener listener;
+  private final BiFunction<Object, VetoableChangeListener, Runnable> listenerBinder;
 
   /** Convenience method to create the set property command. */
   public static <S> SetPropertyCommand<S> create(Property<S> property, S newValue) {
@@ -33,12 +34,12 @@ public class SetPropertyCommand<V> extends AbstractCommand {
       Property<S> property,
       S newValue,
       T listenerTarget,
-      PropertyChangeListener listener,
-      BiFunction<? super T, ? super PropertyChangeListener, Runnable> binder) {
+      VetoableChangeListener listener,
+      BiFunction<? super T, ? super VetoableChangeListener, Runnable> binder) {
 
     @SuppressWarnings("unchecked")
-    BiFunction<Object, PropertyChangeListener, Runnable> erased =
-        (BiFunction<Object, PropertyChangeListener, Runnable>) (BiFunction<?, ?, ?>) binder;
+    BiFunction<Object, VetoableChangeListener, Runnable> erased =
+        (BiFunction<Object, VetoableChangeListener, Runnable>) (BiFunction<?, ?, ?>) binder;
 
     return new SetPropertyCommand<>(property, newValue, listenerTarget, listener, erased);
   }
@@ -71,8 +72,8 @@ public class SetPropertyCommand<V> extends AbstractCommand {
       T object,
       String attributeName,
       V newValue,
-      PropertyChangeListener listener,
-      BiFunction<? super T, ? super PropertyChangeListener, Runnable> binder) {
+      VetoableChangeListener listener,
+      BiFunction<? super T, ? super VetoableChangeListener, Runnable> binder) {
 
     try {
       @SuppressWarnings("unchecked")
@@ -83,8 +84,8 @@ public class SetPropertyCommand<V> extends AbstractCommand {
           .build();
 
       @SuppressWarnings("unchecked")
-      BiFunction<Object, PropertyChangeListener, Runnable> erased =
-          (BiFunction<Object, PropertyChangeListener, Runnable>) (BiFunction<?, ?, ?>) binder;
+      BiFunction<Object, VetoableChangeListener, Runnable> erased =
+          (BiFunction<Object, VetoableChangeListener, Runnable>) (BiFunction<?, ?, ?>) binder;
 
       return new SetPropertyCommand<>(property, newValue, object, listener, erased);
 
@@ -102,8 +103,8 @@ public class SetPropertyCommand<V> extends AbstractCommand {
       Property<V> property,
       V newValue,
       Object listenerTarget,
-      PropertyChangeListener listener,
-      BiFunction<Object, PropertyChangeListener, Runnable> listenerBinder) {
+      VetoableChangeListener listener,
+      BiFunction<Object, VetoableChangeListener, Runnable> listenerBinder) {
 
     this.property = Objects.requireNonNull(property, "property");
     this.newValue = newValue;
@@ -125,6 +126,18 @@ public class SetPropertyCommand<V> extends AbstractCommand {
     try {
       property.setValue(newValue);
       setExecuted(true);
+    } catch (RuntimeException e) { // probably a PropertyVetoException wrapped in a RuntimeException, but we want to be safe about catching only veto-related exceptions
+      if ( e.getCause() instanceof PropertyVetoException) {
+        // Log the veto exception for debugging purposes
+        //System.err.println("Property change was vetoed: " + e.getCause().getMessage());
+      } else {
+        // If it's not a veto exception, rethrow it as it's unexpected
+        throw e;
+      }
+      // If the change was vetoed, we should not mark the command as executed and should not update oldValue.
+      // The command can be retried with a different newValue.
+      setExecuted(false);
+      throw e; // rethrow to allow handling by caller to prevent the command from being added to the stack
     } finally {
       if (unbind != null) {
         unbind.run();
@@ -138,12 +151,31 @@ public class SetPropertyCommand<V> extends AbstractCommand {
       return;
     }
 
+    // Save the current value in case of redo, but restore the old value
+    newValue = property.getValue();
+
     // Add listener -> mutate -> remove listener
     Runnable unbind = bindListenerIfConfigured();
     try {
       property.setValue(oldValue);
       setExecuted(false);
-    } finally {
+    } catch (RuntimeException e) { // probably a PropertyVetoException wrapped in a RuntimeException, but we want to be safe about catching only veto-related exceptions
+      if ( e.getCause() instanceof PropertyVetoException) {
+        // Log the veto exception for debugging purposes
+        //System.err.println("Property change was vetoed: " + e.getCause().getMessage());
+        // this is likely to be "name" so we could mutate the command with a new name and retry, 
+        // but for now we will just leave it to the caller to handle the exception and decide what to do 
+        // (e.g. show an error message and let the user retry with a different value - althouth it's "undo" so the user 
+        // might not bea able to provide a new value, but this is a consequence of the veto and the fact that we want to support redo after undo)
+      } else {
+        // If it's not a veto exception, rethrow it as it's unexpected
+        throw e;
+      }
+      // If the change was vetoed, we should not mark the command as executed and should not update oldValue.
+      // The command can be retried with a different newValue.
+      setExecuted(true); // undo did not succeed, so we are still in the executed state
+      throw e; // rethrow to allow handling by caller to prevent the command from being added to the stack
+   } finally {
       if (unbind != null) {
         unbind.run();
       }
